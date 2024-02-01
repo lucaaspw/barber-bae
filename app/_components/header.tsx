f7f1d476e18f6c4193b7f20782641e9c3ea901a2
lucaaspw <gonclaves.10.lucas@gmail.com>
2024-02-01 10:15:44 -0300

    feat: add header initial

---
+++ app/_components/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <Card className="rounded-none px-5 py-6">
+      <CardContent className="flex justify-between items-center p-0">
+        <Image src="/baebarberlogo.svg" alt="Bae Barber" height={22} width={130} />
+        <Button variant="outline" size="icon">
+          <MenuIcon/>
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Header from "./_components/header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello NLW</h1>
-    </main>
+    <div>
+      <Header/>
+    </div>
   );
 }
